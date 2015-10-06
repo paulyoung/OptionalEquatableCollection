07e9b6bd8ff5f9e2cb11bd46ec6f75850b225208
Paul Young <paulyoungonline@gmail.com>
2015-10-06 12:48:33 -0700

    Update operator definitions to match those in the standard library.

--- OptionalEquatableCollection/Array.swift
+++ OptionalEquatableCollection/Array.swift
@@ -1,6 +1,5 @@
-// Mark: - Equatable
-
-public func == <T: Equatable>(lhs: Array<T>?, rhs: Array<T>?) -> Bool {
+@warn_unused_result
+public func == <Element: Equatable>(lhs: Array<Element>?, rhs: Array<Element>?) -> Bool {
     switch (lhs, rhs) {
     case let (.Some(lhs), .Some(rhs)):
         return lhs == rhs
